185b4167365d1775847fdec9cefff20ad8611a54
mrhy <louis.guerout@ynov.com>
2025-09-11 18:55:17 +0200

    5th commit

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"os"
 	"strings"
-	"time"
 )
 
 type Character struct {
@@ -31,21 +30,19 @@ var c_temp_inventaire []string
 
 func main() {
 	IsFirst := true
-	var input string
 	c1 = initCharacter("Zinrel", "Elfe", 1, 100, 40, []string{"potions de soin", "potions de soin", "potions de soin"})
 	if IsFirst {
-		rand.New(rand.NewSource(time.Now().UnixNano()))
 		min_pt_vie_rand, max_pt_vie_rand := 95, 105
 		fmt.Print("Voulez vous utiliser le personnage par défaut ? Ou voulez vous créer un nouveau personnage ?\n- Pour créer un nouveau personnage veuillez écrire 'Oui'\n- Pour utiliser le personnage par défaut veuillez écrire 'Non'")
 		fmt.Print("\n")
 		displayInfo(c1.nom)
 		fmt.Print("\nVotre réponse : ")
-		fmt.Scan(&input)
+		input := readInput()
 		if input == "Oui" || input == "Oui." || input == "oui" || input == "oui." {
 			fmt.Print("\nVeuillez entrer le nom du personnage : ")
-			fmt.Scan(&c_temp_name)
-			fmt.Print("\nVeuillez entrer la classe du personnage : ") //implémenter la liste des classes
-			fmt.Scan(&c_temp_classe)
+			c_temp_name = readInput()
+			fmt.Print("\nVeuillez entrer la classe du personnage : ")
+			c_temp_classe = readInput()
 			fmt.Printf("\nVotre personnage s'appelle : %s\nLa classe de votre personnage est : %s", c_temp_name, c_temp_classe)
 			fmt.Print("\nVotre personnage commence au niveau 0.")
 			c_temp_niveau = 0
@@ -79,7 +76,7 @@ func initCharacter(nom string, classe string, niveau int, points_de_vie_maximum 
 }
 
 func displayInfo(s string) {
-	s = strings.TrimSpace(s) // TrimSpace supprime tous les espaces ou espacements
+	s = strings.TrimSpace(s)
 	if strings.Contains(strings.ToLower(s), "liste") {
 		fmt.Printf("\nPersonnage 1 :\n   - Nom : %s\n   - Classe : %s\n   - Niveau : %d\n   - Points de vie : %d/%d\n   - Inventaire : %v\n",
 			c1.nom, c1.classe, c1.niveau, c1.points_de_vie_actuels, c1.points_de_vie_maximum, c1.inventaire)
@@ -156,32 +153,32 @@ func takePot(characterName string) {
 func menu() {
 	fmt.Print("\n\nPour sélectionner une option, veuillez entrer le numéro de celle-ci : ")
 	fmt.Print("\n1. Afficher les informations du personnage")
-	fmt.Print("\n2. Accéder au contenu de l’inventaire")
-	fmt.Print("\n3. Quitter")
+	fmt.Print("\n2. Accéder au contenu de l'inventaire")
+	fmt.Print("\n3. Utiliser une potion de soin")
+	fmt.Print("\n4. Quitter")
 	fmt.Print("\n\nVotre réponse : ")
-	var option string
-	fmt.Scan(&option)
-	var commande string
+	option := readInput()
 	switch option {
 	case "1", "1.":
 		fmt.Print("Veuillez sélectionner un personnage de la liste pour afficher ses caractéristiques :\nPour afficher la liste des personnages veuillez entrer 'Liste des personnages'\n")
 		fmt.Print("\nVotre réponse : ")
-		fmt.Scan(&commande)
+		commande := readInput()
 		displayInfo(commande)
 		menu()
 	case "2", "2.":
 		fmt.Print("Veuillez sélectionner un personnage de la liste pour afficher son inventaire :\nPour afficher la liste des personnages veuillez entrer 'Liste des personnages'\n")
 		fmt.Print("\nVotre réponse : ")
-		fmt.Scan(&commande)
+		commande := readInput()
 		accessInventory(commande)
 		menu()
 	case "3", "3.":
 		fmt.Print("Nom du personnage : ")
-		var characterName string
-		fmt.Scan(&characterName)
+		characterName := readInput()
 		takePot(characterName)
 		menu()
 	case "4", "4.":
+		// logique marchand
+	case "5", "5.":
 		return
 	default:
 		fmt.Println("Option invalide")
