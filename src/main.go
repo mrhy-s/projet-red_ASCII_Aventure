3bc8957540add59f1d97bb7c740d1e5bd7f513ce
mrhy <louis.guerout@ynov.com>
2025-09-11 13:59:30 +0200

    first commit

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+type Character struct {
+	nom                   string
+	classe                string
+	niveau                int
+	points_de_vie_maximum int
+	points_de_vie_actuels int
+	inventaire            []string
+}
