c59a7e6b88b833c5c5c1ff34be3ec197b39c2b0d
mrhy <louis.guerout@ynov.com>
2025-09-11 14:44:03 +0200

    second commit

--- src/main.go
+++ src/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Character struct {
 	nom                   string
 	classe                string
@@ -7,4 +9,23 @@ type Character struct {
 	points_de_vie_maximum int
 	points_de_vie_actuels int
 	inventaire            []string
+}
+
+var c1 *Character
+
+func main() {
+	c1 = initCharacter("Zinrel", "Elfe", 1, 100, 40, []string{"potions de soin", "potions de soin", "potions de soin"})
+	fmt.Println(c1)
+}
+
+func initCharacter(nom string, classe string, niveau int, points_de_vie_maximum int, points_de_vie_actuels int, inventaire []string) *Character {
+	character_template := &Character{
+		nom:                   nom,
+		classe:                classe,
+		niveau:                niveau,
+		points_de_vie_maximum: points_de_vie_maximum,
+		points_de_vie_actuels: points_de_vie_actuels,
+		inventaire:            []string{},
+	}
+	return character_template
 }
